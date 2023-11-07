54d7e93dff1e916aab8654b15bcb488552b52f61
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2023-11-08 02:52:13 +0530

    Update Poll.c

--- Poll.c
+++ Poll.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <poll.h>
+
+#define MAX_CLIENTS 4000
+#define PORT 9000
 
 uint64_t fact(int n) {
     if (n <= 1) {
@@ -13,22 +18,20 @@ uint64_t fact(int n) {
     }
 }
 
-
 int main() {
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
+    struct sockaddr_in server_addr;
+    int server_socket, client_sockets[MAX_CLIENTS];
+    struct pollfd poll_fds[MAX_CLIENTS];
+    int num_clients = 0;
 
-    // Create socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
 
-
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(9999);
+    server_addr.sin_port = htons(9901);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     // Bind socket
@@ -45,51 +48,47 @@ int main() {
         exit(EXIT_FAILURE);
     }
     printf("Server Listening\n");
-    struct pollfd pollFD[4000];
-    int count=1;
 
-    pollFD[0].fd=server_socket;
-    pollFD[0].events=POLLIN;
+    // Initialize the poll_fds for server socket
+    poll_fds[0].fd = server_socket;
+    poll_fds[0].events = POLLIN;
+    num_clients++;
 
-    while(1){
-        int pol=poll(pollFD, count, -1);
-        if(pol<0){
-            perror("Poll unsuccessful");
-            return -1;
-        }
+    while (1) {
+        int poll_result = poll(poll_fds, num_clients, -1); // Wait indefinitely for events
 
-        for(int i=0; i<count; i++){
-            if(pollFD[i].revents & POLLIN){
-                if(pollFD[i].fd==server_socket){
-                    int k = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
-                    if(k < 0){
-                        perror("Accept failed");
-                        return -1;
-                    }
-                    pollFD[count].fd=k;
-                    pollFD[count].events=POLLIN;
-                    count++;
-                }else{
-                    char buffer[1000];
-                    memset(&buffer, '\0', 1000);
-                    int readr=read(pollFD[i].fd, buffer, 1000);
-
-                    if(readr<=0){
-                        break;
-                    }
-                    else{
-                        int valRead=atoi(buffer);
-                        getpeername(i, (struct sockaddr*)&client_addr, &client_len);
-                        char buffer_send[1000];
-                        uint64_t ans=fact(valRead);
-
-                        snprintf(buffer_send, 1000, "%ld", ans);
-                        send(pollFD[i].fd, buffer_send, 1000, 0);
-                    }
+        if (poll_fds[0].revents & POLLIN) {
+            if (num_clients < MAX_CLIENTS) {
+                // Accept a new client connection
+                client_sockets[num_clients - 1] = accept(server_socket, NULL, NULL);
+                if (client_sockets[num_clients - 1] != -1) {
+                    poll_fds[num_clients].fd = client_sockets[num_clients - 1];
+                    poll_fds[num_clients].events = POLLIN;
+                    num_clients++;
                 }
             }
         }
 
+        char buffer[1024];
+        for (int i = 1; i < num_clients; i++) {
+        if (poll_fds[i].revents & POLLIN) {
+            int n = read(poll_fds[i].fd, buffer, sizeof(buffer));
+            if (n <= 0) {
+                poll_fds[i] = poll_fds[num_clients - 1];
+                num_clients--;
+            } 
+            else {
+                buffer[n] = '\0';
+	            int num  = atoi(buffer);
+	            if (num > 20) num = 20;
+	            uint64_t result = fact(num);
+	            char response[1024];
+	            snprintf(response, sizeof(response), "Answer: %ld\n", result);
+	            write(poll_fds[i].fd, response, sizeof(response));
+            }
+        }
     }
+    }
+    close(server_socket);
     return 0;
 }
