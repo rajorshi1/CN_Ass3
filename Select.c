4af1a089645361e31a7d17766e004e3db8b954f6
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2023-11-08 04:47:31 +0530

    Update Select.c

--- Select.c
+++ Select.c
@@ -9,82 +9,81 @@
 #include <errno.h>
 #include <netinet/in.h>
 
-uint64_t fact(uint64_t n) {
-    uint64_t result = 1;
-    if (n > 20) n = 20;
-    for (int i = 1; i <= n; i++) {
-        result *= i;
+uint64_t fact(int n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return n * fact(n - 1);
     }
-    return result;
 }
 
 int main() {
-    int sockfd;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("Socket creation failed");
-        exit(1);
+    int socket_server, client_server;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t len = sizeof(client_addr);
+
+    socket_server = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_server == -1) {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in server, client;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(9900);
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    int len = sizeof(struct sockaddr_in);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(9901);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    int bindsock = bind(sockfd, (struct sockaddr*)&server, len);
-    if (bindsock < 0) {
-        perror("Bind failed");
-        exit(1);
+    // Bind socket
+    if (bind(socket_server, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Error binding");
+        close(socket_server);
+        exit(EXIT_FAILURE);
     }
 
-    int listensock = listen(sockfd, 6000);
-    if (listensock < 0) {
-        perror("Listen failed");
-        exit(1);
+    // Listen for incoming connections
+    if (listen(socket_server, 6000) == -1) {
+        perror("Error listening");
+        close(socket_server);
+        exit(EXIT_FAILURE);
     }
+    printf("Server Listening\n");
 
-    int newsockfd;
-    fd_set read_fds, master_fds;
-    int max_fd = sockfd;
+    fd_set read_fds, client_fds;
+    int max_fd = socket_server;
 
-    FD_ZERO(&master_fds);
-    FD_SET(sockfd, &master_fds);
+    FD_ZERO(&client_fds);
+    FD_SET(socket_server, &client_fds);
 
     while (true) {
-        read_fds = master_fds;
+        read_fds = client_fds;
         int num_ready = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
-
-        if (num_ready < 0) {
-            perror("Select failed");
-            exit(1);
-        }
 
         for (int i = 0; i <= max_fd; i++) {
             if (FD_ISSET(i, &read_fds)) {
-                if (i == sockfd) {
-                    newsockfd = accept(sockfd, (struct sockaddr*)&client, &len);
-                    if (newsockfd < 0) {
+                if (i == socket_server) {
+                    client_server = accept(socket_server, (struct sockaddr*)&client_addr, &len);
+                    if (client_server < 0) {
                         perror("Accept failed");
                     } else {
-                        FD_SET(newsockfd, &master_fds);
-                        if (newsockfd > max_fd) {
-                            max_fd = newsockfd;
+                        FD_SET(client_server, &client_fds);
+                        if (client_server > max_fd) {
+                            max_fd = client_server;
                         }
                     }
                 } else {
                     for (int j = 0; j < 50; j++) {
                         char buffer[1024];
-                        ssize_t bytes_received = recv(i, buffer, sizeof(buffer), 0);
-                        if (bytes_received <= 0) {
+                        int n = read(i, buffer, sizeof(buffer));
+                        if (n <= 0) {
                             close(i);
-                            FD_CLR(i, &master_fds);
+                            FD_CLR(i, &client_fds);
                             break;
                         }
-
-                        uint64_t n = strtoull(buffer, NULL, 10);
-                        uint64_t result = fact(n);
-                        send(i, &result, sizeof(result), 0);
+                        buffer[n] = '\0';
+                        uint64_t num = atoi(buffer);
+                        uint64_t result = fact(num);
+                        char response[1024];
+	                    snprintf(response, sizeof(response), "Answer: %ld\n", result);
+	                    write(i, &response, sizeof(response));
                     }
                 }
             }
