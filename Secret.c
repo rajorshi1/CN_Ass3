bff27c7b5381d46dda5cc15e4443aa4d4c5f1a7a
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2023-11-07 17:22:33 +0530

    Update Secret.c

--- Secret.c
+++ Secret.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 uint64_t fact(int n) {
     if (n <= 1) {
@@ -12,7 +13,8 @@ uint64_t fact(int n) {
     }
 }
 
-void handle_client(int client_socket) {
+void *handle_client(void *arg) {
+    int client_socket = *((int *)arg);
     uint64_t n;
     read(client_socket, &n, sizeof(uint64_t));
     if (n > 20) {
@@ -21,12 +23,14 @@ void handle_client(int client_socket) {
     uint64_t result = fact(n);
     write(client_socket, &result, sizeof(uint64_t));
     close(client_socket);
+    pthread_exit(NULL);
 }
 
 int main() {
     int server_socket, client_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
+    pthread_t threads[5];  // Maximum 5 concurrent threads
 
     // Create socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -55,6 +59,7 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    int i = 0;
     while (1) {
         // Accept incoming connection
         client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
@@ -64,13 +69,18 @@ int main() {
             exit(EXIT_FAILURE);
         }
 
-        // Fork a new process to handle the client
-        if (fork() == 0) {
-            close(server_socket);  // Child process doesn't need the listening socket
-            handle_client(client_socket);
-            exit(EXIT_SUCCESS);  // Child process exits after handling client
-        } else {
-            close(client_socket);  // Parent process doesn't need the client socket
+        // Create a new thread to handle the client
+        if (pthread_create(&threads[i++], NULL, handle_client, (void *)&client_socket) != 0) {
+            perror("Error creating thread");
+            close(client_socket);
+        }
+
+        if (i >= 5) {
+            i = 0;  // Reset thread index
+            while (i < 5) {
+                pthread_join(threads[i++], NULL);  // Wait for threads to finish
+            }
+            i = 0;  // Reset thread index after joining threads
         }
     }
 
